886ec4281067b35a6e76a3688f7b217f87663a35
bartek <smalecbartlomiej@gmail.com>
2023-04-25 17:44:44 +0200

    Added examples of sequential processsing and multithreaded execution

--- src/main/java/org/bartek/Main.java
+++ src/main/java/org/bartek/Main.java
@@ -2,6 +2,58 @@ package org.bartek;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Example of sequential processing
+
+        System.out.println("=======================================");
+        System.out.println("Sequential processing. \n");
+
+        RunnerOne runnerOne = new RunnerOne();
+        RunnerTwo runnerTwo = new RunnerTwo();
+
+        runnerOne.execute();
+        runnerTwo.execute();
+
+        // Example of multithreaded execution
+
+        System.out.println("=======================================");
+        System.out.println("Multithreaded execution. \n");
+
+        Thread threadOne = new Thread(new RunnerOne());
+        Thread threadTwo = new Thread(new RunnerTwo());
+
+        threadOne.start();
+        threadTwo.start();
+
+
+    }
+}
+
+class RunnerOne implements Runnable {
+    public void execute() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Runner one: " + i);
+        }
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Runner one: " + i);
+        }
+    }
+}
+
+class RunnerTwo implements Runnable {
+    public void execute() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Runner two: " + i);
+        }
+    }
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Runner two: " + i);
+        }
     }
 }
